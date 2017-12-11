941d244ad64417eb4fcc674cedc70e50f8157bec
tltong <tltong@yahoo.co.uk>
2017-12-11 12:12:36 +0000

    modified auth.ts

--- providers/auth/auth.ts
+++ providers/auth/auth.ts
@@ -1,17 +1,25 @@
-import { HttpClient } from '@angular/common/http';
+import { AngularFireAuth } from 'angularfire2/auth';
+import firebase from 'firebase/app';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the AuthProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class AuthProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello AuthProvider Provider');
+  constructor(public afAuth: AngularFireAuth) { }
+
+  loginUser(newEmail: string, newPassword: string): Promise<any> {
+    return this.afAuth.auth.signInWithEmailAndPassword(newEmail, newPassword);
   }
 
+  resetPassword(email: string): Promise<void> {
+    return this.afAuth.auth.sendPasswordResetEmail(email);
+  }
+
+  logoutUser(): Promise<void> {
+    return this.afAuth.auth.signOut();
+  }
+  
+  signupUser(newEmail: string, newPassword: string): Promise<any> {
+    return this.afAuth.auth.createUserWithEmailAndPassword(newEmail, newPassword);
+  }
 }
