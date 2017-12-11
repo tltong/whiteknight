2054737e0aa4bb5701db3a432247481c8e5c4525
tltong <tltong@yahoo.co.uk>
2017-12-11 13:27:20 +0000

    modified home.html/ts.  Log off works now

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AuthProvider } from '../../providers/auth/auth';
+import { LoginPage } from '../login/login'
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public authData: AuthProvider) {
+    firebase.auth().onAuthStateChanged(function(user) {
+    if (!user) {
+        navCtrl.setRoot(LoginPage);
+      }
+    });
+  }
 
+  logout() {
+    this.authData.logoutUser();
   }
 
 }
