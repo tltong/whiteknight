a613f0e556685d18b5128ccd2e6e87809565879c
tltong <tltong@yahoo.co.uk>
2017-12-12 10:45:20 +0000

    Reset page works now

--- pages/reset-password/reset-password.ts
+++ pages/reset-password/reset-password.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ResetPasswordPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthProvider } from '../../providers/auth/auth';
+import { EmailValidator } from '../../validators/email';
 
 @IonicPage()
 @Component({
@@ -15,8 +11,42 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ResetPasswordPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public resetPasswordForm:FormGroup;
+
+  constructor(public authData: AuthProvider,public formBuilder: FormBuilder,public nav: NavController,public alertCtrl: AlertController) {
+   
+    this.resetPasswordForm = formBuilder.group({
+    email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],  })
   }
+
+  resetPassword(){
+    if (!this.resetPasswordForm.valid){
+      console.log(this.resetPasswordForm.value);
+    } else {
+      this.authData.resetPassword(this.resetPasswordForm.value.email)
+      .then((user) => {
+        let alert = this.alertCtrl.create({
+          message: "We just sent you a reset link to your email",
+          buttons: [            {
+              text: "Ok",
+              role: 'cancel',
+              handler: () => {
+                this.nav.pop();
+              }            }          ]        });
+        alert.present();
+      }, (error) => {
+        var errorMessage: string = error.message;
+        let errorAlert = this.alertCtrl.create({
+          message: errorMessage,
+          buttons: [            {
+              text: "Ok",
+              role: 'cancel'
+            }          ]        });
+        errorAlert.present();
+      });    }
+  }
+
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ResetPasswordPage');
